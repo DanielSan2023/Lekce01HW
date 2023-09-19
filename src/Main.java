3d1f3221bf70b976e2b229c66245d38082bb39d4
Daniel_San <electronic.san@gmail.com>
2023-09-19 12:47:56 +0200

    "I was reviewing code and made some repairs."

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,82 @@
+import java.math.BigDecimal;
+
 public class Main {
+
+    public static void ukol1() {
+       System.out.println("Hello world!");
+    }
+    public static void ukol2() {
+        String uzivatel = "Karel";   //String uzivatel = Karel;
+        System.out.println(uzivatel);
+    }
+
+    public static void ukol3() {
+        int pocetLekci = 10;        // String pocetLekci = 10;
+        System.out.println(pocetLekci);
+    }
+
+    public static void ukol4() {
+        double cena = 10.0;          // double cena = 10,0;
+        System.out.println(cena);
+    }
+
+    public static void ukol5() {       // memory represent numbers in binary(0,1)
+        BigDecimal cena = BigDecimal.ZERO; // Nastavíme do proměnné hodnotu 0
+        // Desetkrát přičteme hodnotu jedna desetina (0,1):
+        for (int i = 0; i < 10; i++) {
+            cena = cena.add(new BigDecimal("0.1"));
+            // hodnotě navíc 0.1
+        }
+        System.out.println(cena);
+        // Má vypsat "1.0"
+    }
+//    public static void ukol5() {
+//        double cena = 0; // Nastavíme do proměnné hodnotu 0
+//        // Desetkrát přičteme hodnotu jedna desetina (0,1):
+//        for (int i = 0; i < 10; i++) {
+//            cena += 0.1;
+//               // operátor "+=" znamená: přičti ke stávající
+//               // hodnotě navíc 0.1
+//        }
+//        System.out.println(cena);
+//        // Má vypsat "1.0". Ale vypíše "0.9999" !!!
+//    }
+
+    public static void ukol6() {
+        BigDecimal cena = BigDecimal.ZERO;          // I had  to use Java class BigDecimal
+        cena = cena.add(BigDecimal.valueOf(0.1));
+        System.out.println(cena);
+    }
+
+    public static void ukol7() {
+        int vykonMotoru = 120;
+        System.out.println("Výkon Motoru je: "+vykonMotoru+" kW.");
+    }
+
+    public static void ukol8() {
+        int velikostKosile = 37;
+        System.out.println("Velikost košile je: " + velikostKosile +".");   // problem with writing java syntax (, ,  +)
+    }
+
+    public static void ukol9() {   // miss static
+        System.out.println("Metoda *skoro* bez chybičky!");
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Oprav chyby a spusť všechny metody!");
+        System.out.println("Komentáře odstraníš klávesovou "
+                +"zkratkou <Ctrl>+</> - použij lomítko "
+                +"na numerické klávesnici.");
+       ukol1();
+       ukol2();
+        ukol3();
+        ukol4();
+        ukol5();
+        ukol6();
+        ukol7();
+        ukol8();
+        ukol9();
+
+
     }
 }
